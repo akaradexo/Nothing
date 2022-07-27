629124097cbbec736263822eed0ab22753552229
Ashutosh Karade <69238714+akaradexo@users.noreply.github.com>
2022-07-27 20:55:49 +0530

    .

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Footer() {
+  return (
+    <Wrap>Footer</Wrap>
+  )
+}
+
+export default Footer
+
+const Wrap =styled.div`
+`
